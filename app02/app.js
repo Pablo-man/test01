9c80ac223415a4e94d76ce3956d97631290b4da2
Pablo Méndez <amandrangopablo@gmail.com>
2025-02-01 01:02:35 -0500

    fix: :grin: Final test :w

--- app02/app.js
+++ app02/app.js
@@ -2,7 +2,7 @@ import express from 'express'
 
 const app = express()
 
-const PORT = process.env.PORT || 3002
+const PORT = process.env.PORT || 4000
 
 app.get("/",(req,res)=> {
     res.json({"message" : "hello gaby"})
